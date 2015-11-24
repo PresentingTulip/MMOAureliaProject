a7c502ba4539acf930a482b2d8e7861891796a15
PresentingTulip <tulipplugins@gmail.com>
2015-11-24 18:22:47 -0500

    First Commit

---
+++ src/xyz/tulipplugins/aureliammo/AureliaMMO.java
@@ -0,0 +1,24 @@
+package xyz.tulipplugins.aureliammo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AureliaMMO extends JavaPlugin {
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+		
+	}
+	
+	
+	/* plan for this plugin 
+	 * 1. Make individual skills
+	 * 2. Create leveling chat system
+	 * 3. Add a sort of GUI reward system.
+	 
+	 */
+
+}
